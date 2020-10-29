a249c33076bd548581f06b4ae0f6d13448c887b1
Oleg Tsarkov <olegtsts@gmail.com>
2020-10-29 10:08:02 +0000

    Using dynamic programming in task2.

--- task2.cpp
+++ task2.cpp
@@ -1,40 +1,34 @@
-#include <iostream> 
+#include <iostream>
 #include <vector>
+#include <string>
+#include <limits>
 #include <algorithm>
 
-std::vector<int> Get(int N) {
-	std::vector<int> vec;
-	int a;
-	while (N != 0) {
-		a = N % 10;
-		vec.push_back(a);
-		N /= 10;
-	}
-    reverse(vec.begin(), vec.end());
-	return vec;
-}
-
-int Min(std::vector<int> vec) {
-	int a = vec[0];
-	for (int i = 0; i < vec.size(); ++i) {
-		if (vec[i] == 0) {
-			a *= 0;
-			return 0;
-		}
-		else if (vec[i] == 1) {
-			a *= 1;
-		}
-		else {
-			a += vec[i];
-		}
-	}
-	return a;
-}
 int main() {
-	int N;
-	std::cin >> N;
-	std::vector<int> V = Get(N);
-	int res = Min(V);
-    std::cout << res;
+    std::string number;
+    std::cin >> number;
+    std::vector<int> digits(number.size());
+    for (size_t i = 0; i < number.size(); ++i) {
+        digits[i] = number[i] - '0';
+    }
+    std::vector<int> prefix_minimums(digits.size() + 1, std::numeric_limits<int>::max());
+    prefix_minimums[0] = 0;
+    prefix_minimums[1] = digits[0];
+    for (size_t prefix_end = 2; prefix_end < digits.size() + 1; ++prefix_end) {
+        for (size_t previous_prefix_end = 0; previous_prefix_end < prefix_end; ++previous_prefix_end) {
+            int new_summand = 1;
+            for (size_t i = previous_prefix_end; i < prefix_end; ++i) {
+                new_summand *= digits[i];
+                if (new_summand > 100000) {
+                    break;
+                }
+            }
+            if (new_summand > 100000) {
+                continue;
+            }
+            prefix_minimums[prefix_end] = std::min(prefix_minimums[prefix_end], prefix_minimums[previous_prefix_end] + new_summand);
+        }
+    }
+    std::cout << prefix_minimums[digits.size()] << std::endl;
 	return 0;
 }
