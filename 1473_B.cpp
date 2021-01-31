56097e73edb6acb84255c0c4e6e03bdf1d37ec00
luizavurts <68293832+luizavurts@users.noreply.github.com>
2021-01-31 21:23:47 +0300

    Update 1473_B.cpp

--- 1473_B.cpp
+++ 1473_B.cpp
@@ -1,55 +1,22 @@
 #include <iostream>
 #include <string>
+#include <numeric>
 
-int NOD(int n1, int n2)
-{
-    int div;
-    if (n1 == n2)  return n1;
-    int d = n1 - n2;
-    if (d < 0) {
-        d = -d;  div = NOD(n1, d);
-    }
-    else
-        div = NOD(n2, d);
-    return div;
-}
-
-int NOK(int n1, int n2)
-{
-    return n1 * n2 / NOD(n1, n2);
-}
-
-bool LCM(std::string s, std::string t) {
+std::string LCM(std::string s, std::string t) {
     int size_1 = s.size();
     int size_2 = t.size();
     std::string new_string_1;
     std::string new_string_2;
-    int nok = NOK(size_1, size_2);
+    int nok = std::lcm(size_1, size_2);
     for (int i = 0; i < nok / size_1; ++i) {
         new_string_1 += s;
     }
     for (int i = 0; i < nok / size_2; ++i) {
         new_string_2 += t;
     }
-    int k = 0;
-    for (int i = 0; i < new_string_1.size(); ++i) {
-        if (new_string_1[i] == new_string_2[i]) {
-            k += 1;
-        }
-    }
-    if (k == nok) {
-        return true;
-    }
-}
-
-std::string string(std::string s, std::string t) {
-        int size_1 = s.size();
-        int size_2 = t.size();
-        std::string new_string;
-        for (int i = 0; i < (NOK(size_1, size_2) / size_2); ++i) {
-            new_string += t;
-        }
-        return new_string;
+    if (new_string_1 == new_string_2) {
+        return new_string_1;
+    }    
 }
 
 int main() {
@@ -58,12 +25,7 @@ int main() {
 	for (int i = 0; i < q; ++i) {
 		std::string s, t;
 		std::cin >> s >> t;
-        if (LCM(s, t) == true) {
-            std::cout << string(s, t) << std::endl;
-        }
-        else {
-            std::cout << -1 << std::endl;
-        }
+        std::cout << LCM(s, t);
     } 
     return 0;
 }
