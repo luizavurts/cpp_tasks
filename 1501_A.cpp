7dc6e71507378efd5c372a8b794f8aa51c9b076a
luizavurts <68293832+luizavurts@users.noreply.github.com>
2021-04-04 20:07:39 +0300

    Update 1501_A.cpp

--- 1501_A.cpp
+++ 1501_A.cpp
@@ -24,10 +24,10 @@ int main() {
 		}
 
 		new_time_of_arrival[0] = a_vector[0] + m_vector[0];
-		new_time_of_departure[0] = new_time_of_arrival[0] + (b_vector[0] - a_vector[0]) / 2 + 1;
+		new_time_of_departure[0] = new_time_of_arrival[0] + (b_vector[0] - a_vector[0] + 1) / 2;
 		for (int j = 1; j < a_vector.size(); ++j) {
 			new_time_of_arrival[j] = new_time_of_departure[j - 1] + m_vector[j] + a_vector[j] - b_vector[j - 1];
-			new_time_of_departure[j] = new_time_of_arrival[j] + (b_vector[j] - a_vector[j]) / 2 + 1;
+			new_time_of_departure[j] = new_time_of_arrival[j] + (b_vector[j] - a_vector[j] + 1) / 2;
 		}
 		std::cout << new_time_of_arrival[n - 1] << std::endl;
 	}
